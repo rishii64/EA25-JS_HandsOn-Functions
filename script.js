eafcb00eff7905310a747409f204e5b224c07704
Saptarsi <saptarsismmitra@gmail.com>
2023-08-27 00:56:50 +0530

    Functions complete

---
+++ script.js
@@ -0,0 +1,81 @@
+// function fs(){
+//     console.log("JS Coding Assignment");
+// }
+// fs()
+
+// function fs(a,b){
+//     let sum = a +b;
+//     console.log('Sum = ',sum);
+// }
+// fs(3,4)
+
+// let value = (a,b)=>{ 
+//     console.log(`Value of ${a} x ${3} = ${a*b}`) 
+// };
+// value(2,3)
+
+// var x=21
+// var girl = function(){
+//     console.log(x)
+//     var x=20
+// }
+// girl();
+
+// var x=21
+// girl()
+// console.log(x)
+// function girl(){
+//     console.log(x)
+//     var x=20
+// }
+
+// var x = 21
+// a();
+// b();
+// function a() {
+//     x = 20;
+//     console.log(x);  //20
+// }
+// function b() {
+//     x = 40;
+//     console.log(x); //40
+// }
+
+// function fact(n){
+//     let fact=1
+//     for(let i=1;i<=n;i++)
+//         fact *=i
+//     return `Factorial of ${n} = ${fact}`;
+// }
+// console.log(fact(5))
+
+// --------------------Day 2
+
+// function FindSum(a, b){
+//     return a + b;
+// }
+// function DisplayData(data, batch){
+//     console.log(`i am from ${data} and My batch is EA${batch}`)
+// }
+// DisplayData("PrepBytes", FindSum(10, 9));
+
+// Abc();
+// const Abc = function(){
+//     let value = 20;
+//     console.log(value);
+// }
+
+// var a=10;
+// (function(){
+//     console.log(a)
+//     var a=20;
+// })();
+
+// const greet = function(name){
+//     return function(m){
+//     console.log( `Hi!! ${name}, ${m}`);
+//     }
+// }
+//     const greet_message = greet('EA19') ;
+//     greet_message("Welcome To PrepBytes")
+//-------------------------------------------------
